654419be5421963817b8c3a6adee34f01a43f86e
viniciusucanutx <viniciuscanutmz@gmail.com>
2024-09-11 14:35:30 -0300

    :bricks: ci: Modificando arquivos no App

--- App.jsx
+++ App.jsx
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { StatusBar } from 'react-native'; 
+import HomeScreen from './src/pages/Home';
+import MovieDetails from './src/pages/Details';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar 
+        barStyle="light-content" 
+        backgroundColor="#011627" 
+      />
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#011627', 
+            borderBottomWidth: 0, 
+          },
+          headerTintColor: '#fff', 
+          headerTitleStyle: {
+            fontWeight: 'bold', 
+          },
+          headerTitleAlign: 'center', 
+        }}
+      >
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{ title: 'Movies' }} 
+        />
+        <Stack.Screen 
+          name="MovieDetails" 
+          component={MovieDetails} 
+          options={{ title: 'Watch' }} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
